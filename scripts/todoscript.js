5f948878668238509b35735bdf5cb9b3007afd3a
Isaac Forson <youngtech303@gmail.com>
2025-02-20 10:45:24 +0000

    working with todo

--- scripts/todoscript.js
+++ scripts/todoscript.js
@@ -1,4 +1,9 @@
-const todo = []
+const todo = ['nake dinner', 'clean room', 'study']
+
+for (let i = 0; i < todo.length; i++){
+const todos = todo[i]
+const html = `<li>${todos}</li>`
+}
 
 function addTodo() {
 const inputElement = document.querySelector('.todoValue')
